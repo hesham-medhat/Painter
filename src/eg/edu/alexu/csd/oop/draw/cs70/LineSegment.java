e05640025b89b21d3768cde98abcc0d1773b7828
EngHesham <heshammedhat5@gmail.com>
2017-10-24 19:26:34 +0200

    Finishing basics of LineSegment

--- src/eg/edu/alexu/csd/oop/draw/cs70/LineSegment.java
+++ src/eg/edu/alexu/csd/oop/draw/cs70/LineSegment.java
@@ -1,8 +1,6 @@
 package eg.edu.alexu.csd.oop.draw.cs70;
 
 import java.awt.Point;
-import java.util.HashMap;
-import java.util.Map;
 
 import eg.edu.alexu.csd.oop.draw.Shape;
 
@@ -34,13 +32,11 @@ public class LineSegment extends Stroke {
 	public LineSegment(final double fpx,
 			final double fpy, final double spx,
 			final double spy) {
-		Map<String, Double> properties =
-				new HashMap<>();
-		properties.put("fpx", fpx);
-		properties.put("fpy", fpy);
-		properties.put("spx", spx);
-		properties.put("spy", spy);
-		prop = properties;
+		super();
+		prop.put("fpx", fpx);
+		prop.put("fpy", fpy);
+		prop.put("spx", spx);
+		prop.put("spy", spy);
 	}
 
 	/**
@@ -49,20 +45,20 @@ public class LineSegment extends Stroke {
 	 * @param sp second point.
 	 */
 	public LineSegment(final Point fp, final Point sp) {
-		Map<String, Double> properties =
-				new HashMap<>();
-		properties.put("fpx", fp.getX());
-		properties.put("fpy", fp.getY());
-		properties.put("spx", sp.getX());
-		properties.put("spy", sp.getY());
-		prop = properties;
+		super();
+		prop.put("fpx", fp.getX());
+		prop.put("fpy", fp.getY());
+		prop.put("spx", sp.getX());
+		prop.put("spy", sp.getY());
 	}
 
 	@Override
 	public Object clone() throws CloneNotSupportedException {
-		Shape newShape = new LineSegment(prop.get("fpx"), prop.get("fpy"), prop.get("spx"), prop.get("spy"));
-		cloneBasic(newShape, this);
-		return newShape;
+		Shape newLineSegment = new LineSegment(prop.get("fpx"),
+				prop.get("fpy"), prop.get("spx"),
+				prop.get("spy"));
+		cloneBasic(newLineSegment, this);
+		return newLineSegment;
 	}
 
 }
